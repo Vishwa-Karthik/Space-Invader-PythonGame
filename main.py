f4323519118ec90f0baa3cba263569ae73579626
vishwa-karthik <vishwa.prarthana@gmail.com>
2022-04-30 19:06:57 +0530

    final

--- main.py
+++ main.py
@@ -8,29 +8,37 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 # background image
-bg = pygame.image.load('bg1.png')
+bg = pygame.image.load('assets/bg1.png')
 
 
 
 # Title & Icon
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('feather.png')
+icon = pygame.image.load('assets/feather.png')
 pygame.display.set_icon(icon)
 
 # player
-playerImg = pygame.image.load('spaceship.png')
+playerImg = pygame.image.load('assets/spaceship.png')
 playerX = 370
 playerY = 500
 playerX_change = 0
 playerY_change = 0
 
 # enemy
-# player
-enemyImg = pygame.image.load('ghost.png')
+enemyImg = pygame.image.load('assets/ghost.png')
 enemyX = random.randint(0,800)
 enemyY = random.randint(50,100)
 enemyX_change = 4
 enemyY_change = 30
+
+# bullets
+bulletsImg = pygame.image.load('assets/bullet.png')
+bulletX = 0
+bulletY = 500
+bulletX_change = 0
+bulletY_change = 2      # bullet speed
+bullet_state = 'ready' # can fire -> cant see bullet on screen
+
 
 
 def player(x,y):
@@ -39,6 +47,10 @@ def player(x,y):
 def enemy(x,y):
     screen.blit(enemyImg,(x,y))
 
+def fire(x,y):
+    global bullet_state
+    bullet_state = 'fire'
+    screen.blit(bulletsImg,(x+10,y+10))
 
 
 # Game Infinite Loop
@@ -55,12 +67,16 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -0.5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 0.5
+            if event.key == pygame.K_SPACE:
+                if bullet_state == 'ready':
+                    bulletX = playerX
+                    fire(bulletX,bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 0
 
 
     playerX += playerX_change
@@ -74,11 +90,19 @@ while running:
     # enemy boundary x axis
     enemyX += enemyX_change
     if enemyX <= 0:
-        enemyX_change = 0.4
+        enemyX_change = 0.3
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -0.4
+        enemyX_change = -0.3
         enemyY += enemyY_change
+
+    # bullet movement
+    if bulletY  <= 0:
+        bulletY = 480
+        bullet_state = 'ready'
+    if bullet_state == 'fire':
+        fire(bulletX,bulletY)
+        bulletY -= bulletY_change
 
 
     player(playerX,playerY)
